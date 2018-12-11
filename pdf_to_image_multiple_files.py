ab79e096e76e271ba90c6079527c5aa75cea62f3
Suhas Pote <suhaspote8@gmail.com>
2018-12-11 17:50:33 +0530

    Add files via upload

---
+++ pdf_to_image_multiple_files.py
@@ -0,0 +1,14 @@
+import os
+from pdf2image import convert_from_path
+rootdir = '/home/cso/Downloads/customer_pdfs'
+
+for subdir, dirs, files in os.walk(rootdir):
+    for file in files:
+        full_path = os.path.join(subdir, file)
+        if full_path.endswith('.pdf'):
+			converted_name = os.path.join(subdir, '%s_conv.jpg' % (file.replace('.pdf', '')))
+			converted_name_2 = os.path.join(subdir, '%s_conv_2.jpg' % (file.replace('.pdf', '')))
+			images = convert_from_path(full_path,500,output_folder=subdir,fmt='jpg', first_page=7, last_page=7)
+			images2 = convert_from_path(full_path,500,output_folder=subdir,fmt='jpg', first_page=9, last_page=9)
+			os.rename(images[0].filename, converted_name)
+			os.rename(images2[0].filename, converted_name_2)
