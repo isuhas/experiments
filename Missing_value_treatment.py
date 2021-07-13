ff7d36bc347875dd3b5b2365db1efab039876869
Suhas Pote <suhaspote8@gmail.com>
2021-07-13 23:48:06 +0530

    Create Missing_value_treatment.py

---
+++ Missing_value_treatment.py
@@ -0,0 +1,15 @@
+# Mode imputation with Groupby for categorical columns
+
+for col in mis_col:
+    try:
+        df[col] = df.groupby('Company_response')[col].transform(lambda x: x.fillna(x.mode()[0] if not x.mode().empty else "Unknown"))
+    except:
+        print("Error --------------------------------------->")
+------------------------------------------------------------------------------------------------       
+df[col] = df.groupby('Company_response')[col].transform(lambda x: x.fillna(x.mode()[0] if not x.mode().empty else "Empty") )
+        
+------------------------------------------------------------------------------------------------
+data11.dropna().groupby("Transaction-Type").transform(lambda x: x.fillna(x.mode() [0]))["Consumer-disputes"] 
+        
+        
+# https://stackoverflow.com/questions/53994621/indexerror-when-replacing-missing-values-with-mode-using-groupby-in-pandas        
